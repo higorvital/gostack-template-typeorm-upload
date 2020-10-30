e93afbfe219b974e2719e031cf0c8d3ebb510104
higorvital <h.vitall96@gmail.com>
2020-10-30 20:45:15 -0300

    desafio completo

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,16 @@
-{
+const{ SnakeNamingStrategy } = require('typeorm-naming-strategies');
+
+module.exports = {
   "type": "postgres",
   "host": "localhost",
   "port": 5432,
   "username": "postgres",
-  "password": "docker",
+  "password": "postgres",
   "database": "gostack_desafio06",
   "entities": ["./src/models/*.ts"],
   "migrations": ["./src/database/migrations/*.ts"],
   "cli": {
     "migrationsDir": "./src/database/migrations"
-  }
+  },
+  "namingStrategy": new SnakeNamingStrategy()
 }
